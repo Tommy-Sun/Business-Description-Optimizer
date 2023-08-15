a26f53428d46bd157f269bd8e04f0465101c30fa
Tommy Kebschull <tkebschull@chatmeter.com>
2023-08-14 17:39:33 -0700

    finish v1 of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import CaptionForm from "./CaptionForm";
 import DateTimePicker from "react-datetime-picker";
 import { Configuration, OpenAIApi } from "openai";
 
-export enum CaptionTypes {
-  NEWOFFER = "New Offer",
-  NEWEVENT = "New Event",
-  OURVALUES = "Our Values As An Organisation",
-}
-
 function App() {
-  const originalBusinessDescription = `Experience the delight of custom-made coffee at your local 7-Eleven, your go-to destination for coffee nearby. At 7-Eleven, we understand that everyone's coffee preference is unique. That's why our self-serve coffee stations are designed to let you craft your perfect cup of coffee. In addition to being your local hub for the best coffee nearby, we offer a broad selection of fresh, high-quality products at an everyday fair price. Furthermore, our fast, convenient, and delicious hot foods are designed to cater to any craving, ensuring that your on-the-go meals are always satisfying. Don't forget to text '7Rewards' to 711711 to download the 7-Eleven app and join 7Rewards for exclusive perks and promotions.`;
-  const address = "30 N. SUMMERLIN AVE, Orlando, FL. USA";
+  const googleOriginalBusinessDescription = `Experience the delight of custom-made coffee at your local 7-Eleven, your go-to destination for coffee nearby. At 7-Eleven, we understand that everyone's coffee preference is unique. That's why our self-serve coffee stations are designed to let you craft your perfect cup of coffee. In addition to being your local hub for the best coffee nearby, we offer a broad selection of fresh, high-quality products at an everyday fair price. Furthermore, our fast, convenient, and delicious hot foods are designed to cater to any craving, ensuring that your on-the-go meals are always satisfying. Don't forget to text '7Rewards' to 711711 to download the 7-Eleven app and join 7Rewards for exclusive perks and promotions.`;
+  const appleOriginalBusinessDescription = `Experience the delight of custom-made coffee at your local 7-Eleven, your go-to destination for coffee nearby. At 7-Eleven, we understand that everyone's coffee preference is unique. That's why our self-serve coffee stations are designed to let you craft your perfect cup of coffee.`;
+  const facebookOriginalBusinessDescription = `Experience the delight of custom-made coffee at your local 7-Eleven, your go-to destination for coffee nearby.`;
+
+  const address = "Orlando, FL.";
   const primaryCategory = "Convenience Store";
   const businessName = "7-eleven";
 
@@ -21,10 +17,6 @@ function App() {
     apiKey: "",
   });
   const openai = new OpenAIApi(configuration);
-
-  const [captionTypeSelected, setCaptionTypeSelected] = useState(
-    CaptionTypes.NEWOFFER
-  );
 
   const [locationKeyWords, setLocationKeyWords] = useState("");
   const [highValueProducts, setHighValueProducts] = useState("");
@@ -34,8 +26,23 @@ function App() {
   const [optimizeGoogle, setOptimizeGoogle] = useState(false);
   const [optimizeFacebook, setOptimizeFacebook] = useState(false);
   const [optimizeApple, setOptimizeApple] = useState(false);
-  const [optimizedBusinessDescription, setOptimizedBusinessDescription] = useState("");
-  console.log("optimized business description: ", optimizedBusinessDescription)
+  const [
+    googleOptimizedBusinessDescription,
+    setGoogleOptimizedBusinessDescription,
+  ] = useState("");
+  const [
+    appleOptimizedBusinessDescription,
+    setAppleOptimizedBusinessDescription,
+  ] = useState("");
+  const [
+    facebookOptimizedBusinessDescription,
+    setFacebookOptimizedBusinessDescription,
+  ] = useState("");
+
+  console.log(
+    "optimized business description: ",
+    googleOptimizedBusinessDescription
+  );
 
   const onSubmit = async () => {
     console.log(mentionPrimaryBusiness);
@@ -50,32 +57,84 @@ function App() {
     );
     // let prompt = `Rewrite and shorten the following business description which is between the curly brackets like so {}: {${originalBusinessDescription}}. Make sure to use the business name: ${businessName} and include appropriate parts of the ${address} and taking into account the category ${primaryCategory}. The output should be a business description between 250 and 750 characters long`;
 
-    let prompt = `Rewrite and shorten the following business description: {${originalBusinessDescription}}. Make sure to use the business name: ${businessName} and include appropriate parts of the ${address} and taking into account the category ${primaryCategory}. The output should be a business description between 250 and 750 characters long`;
-    
-    if(locationKeyWords.length > 0) {prompt += ` Make sure to include the following keywords: ${locationKeyWords}. Place them towards the beginning of the description`};
-
-    if(highValueProducts.length > 0) {prompt += ` Make sure to include a mention of one or more of the high value products: ${highValueProducts}`};
-    
-    if(openingDate != null) {prompt += ` Make sure to include a mention of when the business opened on: ${openingDate.getMonth()}/${openingDate.getDate()}/${openingDate.getFullYear()}`};
-    
-    if(style.length > 0){prompt += ` Make it in the style ${style}.`}
-
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "system", content: prompt }],
-      max_tokens: 180,
-      temperature: 1.0,
-    });
-    const response = completion.data.choices[0].message?.content ?? ""
-    console.log(response.length)
-    
-    setOptimizedBusinessDescription(response);
+    const getCompletion = async (
+      description: string,
+      maxTokens: number,
+      min: number,
+      max: number
+    ) => {
+      let prompt = `Rewrite and shorten the following business description: {${description}}. Make sure to use the business name: ${businessName} and include appropriate parts of the ${address} and taking into account the category ${primaryCategory}.`;
+      if (locationKeyWords.length > 0) {
+        prompt += ` Make sure to include the following keywords: ${locationKeyWords}. Place them towards the beginning of the description`;
+      }
+      if (highValueProducts.length > 0) {
+        prompt += ` Make sure to include a mention of one or more of the high value products: ${highValueProducts}`;
+      }
+      if (openingDate != null) {
+        prompt += ` Make sure to include a mention of when the business opened on: ${openingDate.getMonth()}/${openingDate.getDate()}/${openingDate.getFullYear()}`;
+      }
+      if (style.length > 0) {
+        prompt += ` The response should also be ${style}.`;
+      }
+      prompt += ` Make sure to respond within ${min} and ${max} characters`;
+      return await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "system", content: prompt }],
+        max_tokens: maxTokens,
+        temperature: 1.0,
+      });
+    };
+    let completionGoogle;
+    let completionApple;
+    let completionFacebook;
+    if (optimizeGoogle) {
+      completionGoogle = await getCompletion(
+        googleOriginalBusinessDescription,
+        180,
+        225,
+        725
+      );
+    }
+    if (optimizeApple) {
+      completionApple = await getCompletion(
+        appleOriginalBusinessDescription,
+        125,
+        5,
+        475
+      );
+    }
+    if (optimizeFacebook) {
+      completionFacebook = await getCompletion(
+        facebookOriginalBusinessDescription,
+        38,
+        10,
+        140
+      );
+    }
+    console.log(
+      completionGoogle?.data.choices[0].message?.content ?? "".length
+    );
+    console.log(completionApple?.data.choices[0].message?.content ?? "".length);
+    console.log(
+      completionFacebook?.data.choices[0].message?.content ?? "".length
+    );
+    setGoogleOptimizedBusinessDescription(
+      completionGoogle?.data.choices[0].message?.content ?? ""
+    );
+    setAppleOptimizedBusinessDescription(
+      completionApple?.data.choices[0].message?.content ?? ""
+    );
+    setFacebookOptimizedBusinessDescription(
+      completionFacebook?.data.choices[0].message?.content ?? ""
+    );
   };
 
   return (
     <div className="App">
       <h2 className="header">Optimize Business Description with AI</h2>
-      {optimizedBusinessDescription.length === 0 ? (
+      {googleOptimizedBusinessDescription.length < 1 &&
+      appleOptimizedBusinessDescription.length < 1 &&
+      facebookOptimizedBusinessDescription.length < 1 ? (
         <div className="container">
           <h4>SEO Optimization</h4>
           <input
@@ -156,29 +215,82 @@ function App() {
           <label>Apple (5 to 500 characters)</label>
           <br />
           <br />
-          <button type="submit" onClick={onSubmit}>
-            Optimize
-          </button>
         </div>
       ) : (
         <div className="container">
           <table>
             <tbody>
-
-            <tr>
-              <td>
-            <h4>Original Business Description</h4>
-            <span>{originalBusinessDescription}</span>
-              </td>
-              <td>
-              <h4>Optimized Business Description</h4>
-            <span>{optimizedBusinessDescription}</span>
-              </td>
-            </tr>
+              <tr>
+                <td>
+                  <h4>Original Google Business Description</h4>
+                  <span>{googleOriginalBusinessDescription}</span>
+                </td>
+                <td>
+                  <h4>Optimized Google Business Description</h4>
+                  <textarea rows={10} cols={50}
+                     value={googleOptimizedBusinessDescription}
+                    onChange={(event) =>
+                      setGoogleOptimizedBusinessDescription(event.target.value)
+                    }
+                  />
+                  {googleOptimizedBusinessDescription.length}
+                </td>
+                <td>
+                <button>Cancel</button>
+                <button>Save</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h4>Original Apple Business Description</h4>
+                  <span>{appleOriginalBusinessDescription}</span>
+                </td>
+                <td>
+                  <h4>Optimized Apple Business Description</h4>
+                  <textarea rows={10} cols={50} value={appleOptimizedBusinessDescription} onChange={(event) =>
+                      setAppleOptimizedBusinessDescription(event.target.value)
+                    } />
+                    {appleOptimizedBusinessDescription.length}
+                </td>
+                <td>
+                <button>Cancel</button>
+                <button>Save</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h4>Original Facebook Business Description</h4>
+                  <span>{facebookOriginalBusinessDescription}</span>
+                </td>
+                <td>
+                  <h4>Optimized Facebook Business Description</h4>
+                  <textarea rows={10} cols={50} value={facebookOptimizedBusinessDescription} onChange={(event) =>
+                      setFacebookOptimizedBusinessDescription(event.target.value)
+                    } />
+                    {facebookOptimizedBusinessDescription.length}
+                </td>
+                <td>
+                <button>Cancel</button>
+                <button>Save</button>
+                </td>
+              </tr>
             </tbody>
           </table>
+          <button
+            onClick={() => {
+              setAppleOptimizedBusinessDescription("");
+              setFacebookOptimizedBusinessDescription("");
+              setGoogleOptimizedBusinessDescription("");
+            }}
+          >
+            Back to edit
+          </button>
         </div>
       )}
+      <button type="submit" onClick={onSubmit}>
+        Optimize
+      </button>
+      <div className="container" />
     </div>
   );
 }
